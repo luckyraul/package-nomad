f0a13bf85e9426a5e9127f46141c77a95e6d2f2c
Nikita <nikita@MacBook-Pro-nikita.local>
2025-09-18 15:20:48 +0200

    drop debian11, add debian 13

--- upload.sh
+++ upload.sh
@@ -6,4 +6,4 @@ if [ -f equal ]; then
 fi
 
 RELEASE=$(echo nomad_*.deb)
-deployer upload --distro bullseye,bookworm public_apt "$RELEASE"
+deployer upload --distro bookworm,trixie public_apt "$RELEASE"
